ae815f16f183279f24597934286d6376bd718780
kothwaltejas <tejastk151@gmail.com>
2025-08-06 20:51:26 +0530

    axios

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||'http://localhost:5000/api', // use your backend base URL
+  baseURL: "https://smart-electronics-ne2p.onrender.com/api", // use your backend base URL
 });
 
 // Add a request interceptor to include the auth token
